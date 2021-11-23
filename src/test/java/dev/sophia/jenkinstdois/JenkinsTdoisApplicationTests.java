cc8a0b513c3cc5b505a3255d3aa430494b4c7701
sophiafiama <fiama.sophia.sf@gmail.com>
2021-11-23 09:39:02 -0300

    first commit

---
+++ src/test/java/dev/sophia/jenkinstdois/JenkinsTdoisApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sophia.jenkinstdois;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTdoisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
